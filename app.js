027ed3097a5f720ebed3b92c1bd3fb4e243ba82e
LUIS_MIGUEL_PALACIO_RESTREPO <lpalacio@todo1.net>
2018-09-05 14:10:36 -0500

    server On

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Running...');
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+    console.log('Server On ', __dirname);
+});
+
+app.get('/', (req, res) => {
+
+    console.log('GET /');
+    res.status(200).send("Server On");
+
+});
